f095d3fe90611a6da03abf0620cb478594c1b484
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-07-18 22:57:27 +0200

    add ComponentTreeToImageFilter class
    darcs-hash:20060718205727-2fc9d-3e72d5837e9884a43664c411dc6c5ec58f671a09.gz

---
+++ itkComponentTreeToImageFilter.h
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkComponentTreeToImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2006/03/28 19:59:05 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkComponentTreeToImageFilter_h
+#define __itkComponentTreeToImageFilter_h
+
+#include "itkImageToImageFilter.h"
+
+namespace itk {
+
+/** \class ComponentTreeToImageFilter
+ * \brief 
+ *
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ */
+template<class TInputImage, class TOutputImage>
+class ITK_EXPORT ComponentTreeToImageFilter : 
+    public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ComponentTreeToImageFilter Self;
+  typedef ImageToImageFilter<TInputImage, TOutputImage>
+  Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Some convenient typedefs. */
+  typedef TInputImage InputImageType;
+  typedef TOutputImage OutputImageType;
+  typedef typename InputImageType::Pointer         InputImagePointer;
+  typedef typename InputImageType::ConstPointer    InputImageConstPointer;
+  typedef typename InputImageType::RegionType      InputImageRegionType;
+  typedef typename InputImageType::PixelType       InputImagePixelType;
+  typedef typename OutputImageType::Pointer        OutputImagePointer;
+  typedef typename OutputImageType::ConstPointer   OutputImageConstPointer;
+  typedef typename OutputImageType::RegionType     OutputImageRegionType;
+  typedef typename OutputImageType::PixelType      OutputImagePixelType;
+  typedef typename InputImageType::NodeType        NodeType;
+  typedef typename InputImageType::NodePointer     NodePointer;
+  typedef typename OutputImageType::IndexType      IndexType;
+  
+  /** ImageDimension constants */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  /** Standard New method. */
+  itkNewMacro(Self);  
+
+  /** Runtime information support. */
+  itkTypeMacro(ComponentTreeToImageFilter, 
+               ImageToImageFilter);
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+/*  itkConceptMacro(InputEqualityComparableCheck,
+    (Concept::EqualityComparable<InputImagePixelType>));
+  itkConceptMacro(IntConvertibleToInputCheck,
+    (Concept::Convertible<int, InputImagePixelType>));
+  itkConceptMacro(InputOStreamWritableCheck,
+    (Concept::OStreamWritable<InputImagePixelType>));*/
+  /** End concept checking */
+#endif
+
+protected:
+  ComponentTreeToImageFilter();
+  ~ComponentTreeToImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** ComponentTreeToImageFilter needs the entire input be
+   * available. Thus, it needs to provide an implementation of
+   * GenerateInputRequestedRegion(). */
+  void GenerateInputRequestedRegion() ;
+
+  /** ComponentTreeToImageFilter will produce the entire output. */
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+  
+  /** Single-threaded version of GenerateData.  This filter delegates
+   * to GrayscaleGeodesicErodeImageFilter. */
+	  void GenerateData();
+
+  void WriteNodes( const NodeType* );
+  
+
+private:
+  ComponentTreeToImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+} ; // end of class
+
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkComponentTreeToImageFilter.txx"
+#endif
+
+#endif
+
+
---
+++ itkComponentTreeToImageFilter.txx
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkComponentTreeToImageFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2005/08/23 15:09:03 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkComponentTreeToImageFilter_txx
+#define __itkComponentTreeToImageFilter_txx
+
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkComponentTreeToImageFilter.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+#include <algorithm>
+#include <list>
+#include "itkPreOrderTreeIterator.h"
+
+
+namespace itk {
+
+template <class TInputImage, class TOutputImage>
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::ComponentTreeToImageFilter()
+{
+}
+
+template <class TInputImage, class TOutputImage>
+void 
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+  
+  // We need all the input.
+  InputImagePointer input = const_cast<InputImageType *>(this->GetInput());
+  
+  if ( !input )
+    { return; }
+
+  input->SetRequestedRegion( input->GetLargestPossibleRegion() );
+}
+
+
+template <class TInputImage, class TOutputImage>
+void 
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()
+    ->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::GenerateData()
+{
+  // Allocate the output
+  this->AllocateOutputs();
+this->GetOutput()->FillBuffer(255);
+  this->WriteNodes( this->GetInput()->GetRoot() );
+    
+  
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::WriteNodes( const NodeType* node )
+{
+	assert(node != NULL);
+	OutputImagePixelType v = node->GetPixel();
+	const typename NodeType::IndexListType * indexList = & node->GetIndexList();
+	OutputImageType* output = this->GetOutput();
+  for( typename NodeType::IndexListType::const_iterator it=indexList->begin(); it!=indexList->end(); it++ )
+    {
+	  std::cout << *it << ": " << v << std::endl;
+	  output->SetPixel( *it, v );
+    }
+  
+  const typename NodeType::ChildrenListType * childrenList = & node->GetChildrenList();
+	for( typename NodeType::ChildrenListType::const_iterator it=childrenList->begin(); it!=childrenList->end(); it++ )
+    {
+		this->WriteNodes( *it );
+	  }
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ComponentTreeToImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+}
+  
+}// end namespace itk
+#endif
