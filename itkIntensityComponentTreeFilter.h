3c59f182de3485cd0f4b44c31b5903499d1afb25
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-08-02 17:31:11 +0200

    add intensity attribute
    darcs-hash:20060802153111-2fc9d-0aa311f634ff1d20b07e961e42e17c8b57507d09.gz

---
+++ itkIntensityComponentTreeFilter.h
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkIntensityComponentTreeFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2006/03/28 19:59:05 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkIntensityComponentTreeFilter_h
+#define __itkIntensityComponentTreeFilter_h
+
+#include "itkInPlaceComponentTreeFilter.h"
+
+namespace itk {
+/** \class IntensityComponentTreeFilter
+ * \brief 
+ *
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ */
+template<class TInputImage, class TIntensity=typename TInputImage::AttributeType>
+class ITK_EXPORT IntensityComponentTreeFilter : 
+    public InPlaceComponentTreeFilter<TInputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef IntensityComponentTreeFilter Self;
+  typedef InPlaceComponentTreeFilter<TInputImage>
+  Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Some convenient typedefs. */
+  typedef TInputImage InputImageType;
+  typedef TInputImage TOutputImage;
+  typedef TOutputImage OutputImageType;
+  typedef typename InputImageType::Pointer         InputImagePointer;
+  typedef typename InputImageType::ConstPointer    InputImageConstPointer;
+  typedef typename InputImageType::RegionType      InputImageRegionType;
+  typedef typename InputImageType::PixelType       InputImagePixelType;
+  typedef typename OutputImageType::Pointer        OutputImagePointer;
+  typedef typename OutputImageType::ConstPointer   OutputImageConstPointer;
+  typedef typename OutputImageType::RegionType     OutputImageRegionType;
+  typedef typename OutputImageType::PixelType      OutputImagePixelType;
+  typedef typename OutputImageType::NodeType       NodeType;
+  typedef typename OutputImageType::IndexType      IndexType;
+  typedef TIntensity IntensityType;
+  
+  /** ImageDimension constants */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  /** Standard New method. */
+  itkNewMacro(Self);  
+
+  /** Runtime information support. */
+  itkTypeMacro(IntensityComponentTreeFilter, 
+               ImageToImageFilter);
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+/*  itkConceptMacro(InputEqualityComparableCheck,
+    (Concept::EqualityComparable<InputImagePixelType>));
+  itkConceptMacro(IntConvertibleToInputCheck,
+    (Concept::Convertible<int, InputImagePixelType>));
+  itkConceptMacro(InputOStreamWritableCheck,
+    (Concept::OStreamWritable<InputImagePixelType>));*/
+  /** End concept checking */
+#endif
+
+protected:
+  IntensityComponentTreeFilter();
+  ~IntensityComponentTreeFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** Single-threaded version of GenerateData.  This filter delegates
+   * to GrayscaleGeodesicErodeImageFilter. */
+  void GenerateData();
+  
+  void SetComponentIntensity( NodeType* );
+
+private:
+  IntensityComponentTreeFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+} ; // end of class
+
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkIntensityComponentTreeFilter.txx"
+#endif
+
+#endif
+
+
---
+++ itkIntensityComponentTreeFilter.txx
@@ -0,0 +1,76 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkIntensityComponentTreeFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2005/08/23 15:09:03 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkIntensityComponentTreeFilter_txx
+#define __itkIntensityComponentTreeFilter_txx
+
+#include "itkIntensityComponentTreeFilter.h"
+#include "itkProgressReporter.h"
+
+
+namespace itk {
+
+template <class TInputImage, class TSize>
+IntensityComponentTreeFilter<TInputImage, TSize>
+::IntensityComponentTreeFilter()
+{
+}
+
+
+template<class TInputImage, class TSize>
+void
+IntensityComponentTreeFilter<TInputImage, TSize>
+::GenerateData()
+{
+  // Allocate the output
+  this->AllocateOutputs();
+
+  ProgressReporter progress(this, 0, this->GetOutput()->GetRequestedRegion().GetNumberOfPixels()*2);
+  this->SetComponentIntensity( this->GetOutput()->GetRoot() );
+
+  // TODO: how to generate progress ??
+
+}
+
+
+template<class TInputImage, class TSize>
+void
+IntensityComponentTreeFilter<TInputImage, TSize>
+::SetComponentIntensity( NodeType* node )
+{
+  assert(node != NULL);
+  IntensityType intensity = NumericTraits<IntensityType>::Zero;
+  const typename NodeType::ChildrenListType * childrenList = & node->GetChildren();
+  for( typename NodeType::ChildrenListType::const_iterator it=childrenList->begin(); it!=childrenList->end(); it++ )
+    {
+    this->SetComponentIntensity( *it );
+    IntensityType i = (*it)->m_Attribute + ( (*it)->GetPixel() - node->GetPixel() );
+    intensity = std::max( intensity, i );
+    }
+  node->m_Attribute = intensity;
+}
+
+
+template<class TInputImage, class TSize>
+void
+IntensityComponentTreeFilter<TInputImage, TSize>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+  
+}// end namespace itk
+#endif
