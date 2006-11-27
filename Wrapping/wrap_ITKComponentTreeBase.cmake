537fb706ce67297b5a8f17178100b5e4ed3fc4a6
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-11-27 16:49:23 +0100

    wrap types required for ComponentTreeAttributeToImageFilter
    darcs-hash:20061127154923-2fc9d-809912013a7183e2027d8bef9975b8aaf4a2669b.gz

--- Wrapping/wrap_ITKComponentTreeBase.cmake
+++ Wrapping/wrap_ITKComponentTreeBase.cmake
@@ -19,6 +19,17 @@ WRAP_CLASS("itk::ImageToImageFilter" POINTER)
       # component tree -> image
       WRAP_TEMPLATE("${ITKM_CT${t}${d}UL}${ITKM_I${t}${d}}" "${ITKT_CT${t}${d}UL}, ${ITKT_I${t}${d}}")
       WRAP_TEMPLATE("${ITKM_CT${t}${d}D}${ITKM_I${t}${d}}" "${ITKT_CT${t}${d}D}, ${ITKT_I${t}${d}}")
+
+      IF(NOT WRAP_unsigned_long)
+        WRAP_TEMPLATE("${ITKM_CT${t}${d}UL}${ITKM_IUL${d}}" "${ITKT_CT${t}${d}UL}, ${ITKT_IUL${d}}")
+      ENDIF(NOT WRAP_unsigned_long)
+
+      FOREACH(t2 ${WRAP_ITK_REAL})
+        IF(NOT t1 STREQUAL t2)
+          WRAP_TEMPLATE("${ITKM_CT${t}${d}D}${ITKM_I${t2}${d}}" "${ITKT_CT${t}${d}D}, ${ITKT_I${t2}${d}}")
+        ENDIF(NOT t1 STREQUAL t2)
+      ENDFOREACH(t2)
+
     ENDFOREACH(d)
   ENDFOREACH(t)
 END_WRAP_CLASS()
