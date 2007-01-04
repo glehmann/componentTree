d2be64ab64660664137c09de69d895ff6d3066a5
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-04 18:33:18 +0100

    add size opening test
    darcs-hash:20070104173318-2fc9d-5dcea1c493702981dbacfdcd97bfb709a7d5b953.gz

---
+++ size_opening.cxx
@@ -0,0 +1,56 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkComponentTree.h"
+#include "itkImageToMaximumTreeFilter.h"
+#include "itkSizeComponentTreeFilter.h"
+#include "itkAttributeFilteringComponentTreeToImageFilter.h"
+
+int main(int argc, char * argv[])
+{
+  if( argc != 5 )
+    {
+    std::cerr << "usage: " << argv[0] << " inputImage outputImage connectivity size" << std::endl;
+    std::cerr << "  inputImage: an input image (up to dim=3)." << std::endl;
+    std::cerr << "  outputImage: the value of the attribute for all the pixels, with float type." << std::endl;
+    std::cerr << "  connectivity: 1 for fully connected, or 0" << std::endl;
+    std::cerr << "  size: the size of the components to remove" << std::endl;
+    exit(1);
+    }
+    
+  const int dim = 3;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ComponentTree< PType, dim, unsigned long > TreeType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::ImageToMaximumTreeFilter< IType, TreeType > MaxTreeType;
+  MaxTreeType::Pointer maxtree = MaxTreeType::New();
+  maxtree->SetInput( reader->GetOutput() );
+  maxtree->SetFullyConnected( atoi( argv[3] ) );
+
+  typedef itk::SizeComponentTreeFilter< TreeType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( maxtree->GetOutput() );
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::AttributeFilteringComponentTreeToImageFilter< TreeType, IType > T2IType;
+  T2IType::Pointer filter2 = T2IType::New();
+  filter2->SetInput( filter->GetOutput() );
+  filter2->SetThreshold( atoi( argv[4] ) );
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter2->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
