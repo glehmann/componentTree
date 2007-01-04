6e1044508b61c55848a89b52892976f508255999
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-04 12:02:19 +0100

    add size tests
    darcs-hash:20070104110219-2fc9d-79b045c3474f0a6eb00b5e7272281182f750a0f6.gz

---
+++ size.cxx
@@ -0,0 +1,54 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkComponentTree.h"
+#include "itkImageToMaximumTreeFilter.h"
+#include "itkSizeComponentTreeFilter.h"
+#include "itkComponentTreeAttributeToImageFilter.h"
+
+int main(int argc, char * argv[])
+{
+  if( argc != 4 )
+    {
+    std::cerr << "usage: " << argv[0] << " inputImage outputImage connectivity" << std::endl;
+    std::cerr << "  inputImage: an input image (up to dim=3). The size of the image must be smaller than 65536 pixel to give correct output." << std::endl;
+    std::cerr << "  outputImage: the value of the attribute for all the pixels" << std::endl;
+    std::cerr << "  connectivity: 1 for fully connected, or 0" << std::endl;
+    exit(1);
+    }
+    
+  const int dim = 3;
+  
+  typedef unsigned short PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ComponentTree< PType, dim, PType > TreeType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::ImageToMaximumTreeFilter< IType, TreeType > MaxTreeType;
+  MaxTreeType::Pointer maxtree = MaxTreeType::New();
+  maxtree->SetInput( reader->GetOutput() );
+  maxtree->SetFullyConnected( atoi( argv[3] ) );
+
+  typedef itk::SizeComponentTreeFilter< TreeType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( maxtree->GetOutput() );
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::ComponentTreeAttributeToImageFilter< TreeType, IType > T2IType;
+  T2IType::Pointer filter2 = T2IType::New();
+  filter2->SetInput( filter->GetOutput() );
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter2->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
