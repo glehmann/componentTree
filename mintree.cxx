bcfdcfcce821f70eba5fbedbe3bf7f555f1081f0
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-04 11:24:10 +0100

    add very simple tests for max and min tree creation
    darcs-hash:20070104102410-2fc9d-d081715ecd7308989b1936339b2b50d50b27e6e1.gz

---
+++ mintree.cxx
@@ -0,0 +1,50 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkComponentTree.h"
+#include "itkImageToMinimumTreeFilter.h"
+#include "itkComponentTreeToImageFilter.h"
+
+int main(int argc, char * argv[])
+{
+  if( argc != 4 )
+    {
+    std::cerr << "usage: " << argv[0] << " inputImage outputImage connectivity" << std::endl;
+    std::cerr << "  inputImage: an input image (up to dim=3)" << std::endl;
+    std::cerr << "  outputImage: the output image" << std::endl;
+    std::cerr << "  connectivity: 1 for fully connected, or 0" << std::endl;
+    exit(1);
+    }
+    
+  const int dim = 3;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ComponentTree< PType, dim, unsigned long > TreeType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::ImageToMinimumTreeFilter< IType, TreeType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetFullyConnected( atoi( argv[3] ) );
+  itk::SimpleFilterWatcher watcher(filter, "min-tree");
+
+  typedef itk::ComponentTreeToImageFilter< TreeType, IType > T2IType;
+  T2IType::Pointer filter2 = T2IType::New();
+  filter2->SetInput( filter->GetOutput() );
+  itk::SimpleFilterWatcher cWatcher(filter2, "tree to img");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter2->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
